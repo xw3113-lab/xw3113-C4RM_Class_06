b1319f368b27edfd4a864cf861ae54c2c19307c6
xw3113-lab <xw3113@columbia.edu>
2026-03-08 21:10:56 -0400

    Add unit tests for ES function with confidence and VaR

--- ES_File.py
+++ ES_File.py
@@ -11,3 +11,15 @@ def ES(losses, confidence=None, VaR=None, use_PnL=False):
 
     es_value = np.mean(losses[losses > VaR])
     return es_value
+
+
+# Unit test
+u = np.random.uniform(0, 100, 100000)
+
+# Test the ES function with an confidence of 0.8
+es_confidence = ES(losses=u, confidence=0.8)
+print('ES with confidence:', np.round(es_confidence, 0) == 90)
+
+# Test the ES function with a VaR of 80
+es_var = ES(losses=u, VaR=80)
+print('ES with VaR:', np.round(es_var, 0) == 90)
