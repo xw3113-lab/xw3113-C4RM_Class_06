5ca1990234cc3f9f7d84fbb650d42c44a29c8ba8
xw3113-lab <xw3113@columbia.edu>
2026-03-08 21:09:45 -0400

    Update VaR_File.py

--- VaR_File.py
+++ VaR_File.py
@@ -1,10 +1,38 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 def VaR(r, confidence, principal=1):
-    # This function returns the left tail value
-    # confidence = certaint that losses are not greater than the VaR value
-    # r = an array of stock returns
-    # out = positively stated value of r at the 1-confidence percentile * principal
+    # This function returns the left tail value and displays a histogram
+    # r = a vector of stock returns
+    # principal = investment initial value
+    # out = principal * positively stated value of r at the 1-alpha percentile
+    out = principal * percent_var(r, confidence)
+    return out
 
-    out = .01 # Calculate the percentile
-    return out  # Return the absolute value of the calculated percentile
+
+# Partial demonstration
+def percent_var(r, confidence):
+    # This function returns the left tail value and displays a histogram
+    # r = a vector of stock percent returns
+    # out = positively stated value of r at the 1-alpha percentile
+
+    plt.hist(r, bins=50, alpha=0.75)
+    plt.show()
+
+    alpha = confidence
+    out = np.percentile(r, (1 - alpha) * 100)
+    return abs(out)
+
+
+# Example tools: percentile
+returns = np.random.normal(0, 1, 10000)
+print(np.percentile(returns, 97.72))
+
+# Unit test
+r = np.random.normal(0.05, 0.03, 1000000)
+probability2SD = norm.cdf(2)  # Probability under normal curve within 2 standard deviations
+
+my_confidence = probability2SD
+my_percent_var = percent_var(r, my_confidence)
+print(np.round(my_percent_var, 2) == 0.01)
